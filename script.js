e34eff0f274d553f9d39ce0ca6edc29099458745
CoderCat010 <nusraumme@gmail.com>
2026-04-11 21:43:42 +0600

    added all products data

---
+++ script.js
@@ -0,0 +1,68 @@
+// all products data
+let products = {
+  data: [
+    // product: 1
+    {
+      productName: "Regular White T-Shirt",
+      category: "Topwear",
+      price: "30",
+      image: "white-tshirt.jpg",
+    },
+
+    // product: 2
+    {
+      productName: "Beige Short Skirt",
+      category: "Bottomwear",
+      price: "49",
+      image: "short-skirt.jpg",
+    },
+
+    // product: 3
+    {
+      productName: "Sporty SmartWatch",
+      category: "Watch",
+      price: "99",
+      image: "sporty-smartwatch.jpg",
+    },
+
+    // product: 4
+    {
+      productName: "Basic Knitted Top",
+      category: "Topwear",
+      price: "29",
+      image: "knitted-top.jpg",
+    },
+
+    // product: 5
+    {
+      productName: "Black Leather Jacket",
+      category: "Jacket",
+      price: "129",
+      image: "black-leather-jacket.jpg",
+    },
+
+    // product: 6
+    {
+      productName: "Stylish Pink Trousers",
+      category: "Bottomwear",
+      price: "89",
+      image: "pink-trousers.jpg",
+    },
+
+    // product: 7
+    {
+      productName: "Brown Men's Jacket",
+      category: "Jacket",
+      price: "189",
+      image: "brown-jacket.jpg",
+    },
+
+    // product: 8
+    {
+      productName: "Comfy Gray Pants",
+      category: "Bottomwear",
+      price: "49",
+      image: "comfy-gray-pants.jpg",
+    },
+  ],
+};
